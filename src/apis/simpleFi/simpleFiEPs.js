227153aa32ca7dd927a5438a9ffdf0f7d33581c9
Raphael <raphael.mazet@gmail.com>
2021-01-19 18:17:27 +0000

    add dev environment

--- src/apis/simpleFi/simpleFiEPs.js
+++ src/apis/simpleFi/simpleFiEPs.js
@@ -1,4 +1,5 @@
-const baseUrl = 'https://simplefi-server.herokuapp.com';
+const nodeEnv = process.env.NODE_ENV;
+const baseUrl = nodeEnv === 'production' ? 'https://simplefi-server.herokuapp.com' : 'http://localhost:3020';
 const fieldEP = '/fields';
 const tokensEP = '/tokens';
 const fieldTokensEP = '/userfieldtokens';
