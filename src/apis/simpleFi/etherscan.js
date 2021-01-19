da1ce6e47d43f883dce5f3dca6d2c646b45e4999
Raphael <raphael.mazet@gmail.com>
2021-01-19 18:53:01 +0000

    add readme and amend github link

--- src/apis/simpleFi/etherscan.js
+++ src/apis/simpleFi/etherscan.js
@@ -1,6 +1,5 @@
 import fetchRequest from '../fetchRequest';
 import {baseUrl, userTxEP} from './simpleFiEPs';
-// TODO: add this requirement to Github documentation
 
 async function getUserTransactions(address){
   const userTransactions = await fetchRequest(baseUrl + userTxEP + '/' + address);
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -41,7 +41,7 @@ export default function Nav ({splash, userAccount, history}) {
         <div className="nav-links">
           <p ref={aboutRef} >About</p>
           <p>😊</p>
-          <a href="https://github.com/raphael-mazet/SimpleFi" target="_blank" rel="noreferrer">Github</a>
+          <a href="https://github.com/raphael-mazet/simplefi-client" target="_blank" rel="noreferrer">Github</a>
         </div>
           <div className="nav-address-button" style={!splash ? {visibility:'hidden'} : {}}>
             <p>{userAccount.length ? `${userAccount[0].slice(0,6)}...${userAccount[0].slice(-4)}` : '0x0000...0000'}</p>
