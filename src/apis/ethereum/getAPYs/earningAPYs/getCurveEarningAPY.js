756b11687924e367a4fc2db80f6af3b391fde13e
Raphael <raphael.mazet@gmail.com>
2021-01-22 11:42:47 +0000

    add Curve stEth pool APY

--- src/apis/ethereum/getAPYs/earningAPYs/getCurveEarningAPY.js
+++ src/apis/ethereum/getAPYs/earningAPYs/getCurveEarningAPY.js
@@ -6,6 +6,8 @@ async function getCurveEarningAPY (field) {
   const { name } = field;
   const latestAPYs = await getAllCurvePoolRawAPY();
   const epKey = indivPoolEPs[name];
+  console.log(' ---> name', name);
+  console.log(' ---> latestAPYs.apy.day[epKey]', latestAPYs.apy.day[epKey]);
   return latestAPYs.apy.day[epKey];
 }
 
