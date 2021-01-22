269fb712b713d63fd62637baf8572b9a2cbdcefc
Raphael <raphael.mazet@gmail.com>
2021-01-22 12:26:15 +0000

    fix decimal divisor bug for Eth in getCurveLiquidityHistory

--- src/apis/ethereum/getAPYs/earningAPYs/getCurveEarningAPY.js
+++ src/apis/ethereum/getAPYs/earningAPYs/getCurveEarningAPY.js
@@ -6,8 +6,6 @@ async function getCurveEarningAPY (field) {
   const { name } = field;
   const latestAPYs = await getAllCurvePoolRawAPY();
   const epKey = indivPoolEPs[name];
-  console.log(' ---> name', name);
-  console.log(' ---> latestAPYs.apy.day[epKey]', latestAPYs.apy.day[epKey]);
   return latestAPYs.apy.day[epKey];
 }
 
