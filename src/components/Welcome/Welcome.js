305ded5df07e37ca2899a80d84c400138ad22f4f
Raphael <raphael.mazet@gmail.com>
2021-01-26 22:44:44 +0000

    add any account functionality

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { ethers } from 'ethers';
 import simpleFiSplash from '../../assets/images/simpleFi-splash-blue-sun.svg';
 import simpleFiLogo from '../../assets/logos/simplefi-logotype.svg';
 import './Welcome.css';
@@ -7,7 +8,40 @@ import { useHistory } from 'react-router-dom';
 
 export default function Welcome ({setUserAccount, userAccount, setSplash}) {
 
+  const [accountValue, setAccountValue] = useState('');
   const history = useHistory();
+  const accountFormRef = useRef(null);
+
+  function toggleForm(e, ref) {
+    e.preventDefault();
+    const form = ref.current.style;
+    const button = e.target.classList;
+    if (form?.display === '' || form?.display === 'none') {
+      form.display = 'block';
+      form.animation = 'growDown 300ms ease-in-out forwards';
+      button.add('visible-add-account');
+      button.remove('invisible-add-account');
+    } else {
+      form.animation = 'shrinkUp 300ms ease-in-out forwards';
+      setTimeout(() => form.display = 'none', 300);
+      button.remove('visible-add-account');
+      button.add('invisible-add-account');
+    }
+  }
+
+  function handleChange(e) {
+    setAccountValue(e.target.value)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (ethers.utils.isAddress(accountValue)) {
+      setUserAccount([accountValue]);
+      history.push('/dashboard');
+    } else {
+      alert('Please enter a valid Ethereum address')
+    }
+  }
 
   useEffect(() => {
     setSplash(false);
@@ -26,6 +60,14 @@ export default function Welcome ({setUserAccount, userAccount, setSplash}) {
         </div>
         <div className="splash-connect">
             <button className='welcome-button' onClick={() => connectWallet(setUserAccount, history, userAccount)}>{userAccount[0] ? 'View dashboard' : 'Connect wallet'}</button>
+            <button className='alt-connect-button' onClick={(e) => toggleForm(e, accountFormRef)}>or check address</button>
+            <form ref={accountFormRef} type="text" value={accountValue} onSubmit={handleSubmit}>
+              <label>
+                Name:
+                <input type="text" name="name" onChange={handleChange}/>
+              </label>
+              <input type="submit" value="Submit" />
+            </form>
         </div>
       </div>
       <div className="welcome-media">
