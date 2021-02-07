89d9a4f2e640f81145b5d34517d23b47953cb980
Raphael <raphael.mazet@gmail.com>
2021-02-07 17:35:41 +0000

    Aave pools: fix balance display bug due to continuously update user balances

--- src/helpers/ethHelpers/ethROIHelpers/createWhitelist.js
+++ src/helpers/ethHelpers/ethROIHelpers/createWhitelist.js
@@ -10,9 +10,10 @@
 function createWhitelist(trackedFields, field) {
   const whitelist = [];
   trackedFields.forEach(trackedField => {  
-    /* @dev: checking !trackedField.isEarning excludes staking/providing liquidity to an earning field with 
-             another earning field's receipt tokens. This avoids double counting ROI. It is a similar check
-             (and necessary/complementary) to Rewinder()'s excludeFeeder flag
+    /* @dev: - checking !trackedField.isEarning excludes staking/providing liquidity to an earning field with 
+               another earning field's receipt tokens. This avoids double counting ROI.
+               It is a similar check(and necessary/complementary) to Rewinder()'s excludeFeeder flag
+               The side effect is that it won't be considered a "staking" event in the field's details table
     */
     if (!trackedField.isEarning) {
       trackedField.seedTokens.forEach(seedToken => {
