70d9ba63aa719afc839d4b1ea451bee1f2ea56a6
Raphael <raphael.mazet@gmail.com>
2021-02-12 13:00:29 +0000

    finalise full relative profit calc for Uniswap

--- src/helpers/ethHelpers/ethROIHelpers/calcEarningROI.js
+++ src/helpers/ethHelpers/ethROIHelpers/calcEarningROI.js
@@ -8,10 +8,12 @@
 function calcEarningROI (currInvestmentValue, txHistory, field, tokenPrices) {
   let valueInvested = 0;
   let valueRealised = 0;
-  //NOTE: histFieldReserves, feeData and feeValueRealised only valid for Uniswap for now
-  let feeValueRealised = 0;
+  //NOTE: histFieldReserves, feeData and realisedRelativeProfitValue only valid for Uniswap for now
+  let realisedRelativeProfitValue = 0;
+  let unrealisedRelativeProfitValue = 0;
+  let totalRelativeROI = 0;
   let userReceiptBalanceAfterLastTx = 0;
-  let reservesAdded = [0,0];
+  let seedTokensInvested = [0,0];
   
   txHistory.forEach(tx => {
     const { txIn, txOut, pricePerToken, histFieldReserves } = tx;
@@ -19,31 +21,32 @@ function calcEarningROI (currInvestmentValue, txHistory, field, tokenPrices) {
       txIn ? valueInvested += txIn * pricePerToken : valueRealised += txOut * pricePerToken;
 
       if (histFieldReserves) {
-        calcRelativeProfit(tx);
+        calcRealisedRelativeProfit(tx);
       }
     }
     //NOTE: valid for Uniswap - assumes just 2 reserve tokens
   })
   const absReturnValue = currInvestmentValue + valueRealised - valueInvested;
   const allTimeROI = ((currInvestmentValue + valueRealised) / valueInvested) - 1;
-  console.log(' ---> feeValueRealised', feeValueRealised);
-  console.log(' ---> reservesAdded', reservesAdded);
-  console.log(' ---> field', field);
+
+  //TODO: clean this spaghetti up and add to parent instead
+  if (field.name === "Uni: MTA-wETH 50/50") {
+    calcUnrealisedRelativeProfit();
+    totalRelativeROI = (realisedRelativeProfitValue + unrealisedRelativeProfitValue) / valueInvested;
+  }
   
-  
-  return {allTimeROI, absReturnValue, histInvestmentValue: valueInvested};
+  return {allTimeROI, absReturnValue, histInvestmentValue: valueInvested, relativeProfitValue: {realisedProfitValue: realisedRelativeProfitValue, unrealisedProfitValue: unrealisedRelativeProfitValue, totalRelativeROI}};
 
 
-  function calcRelativeProfit (tx) {
+  function calcRealisedRelativeProfit (tx) {
     const { txIn, txOut, pricePerToken, histFieldReserves } = tx;
     const {receiptTokenTotalSupply, reserves} = histFieldReserves;
     if (txIn) {
       const additionalUserReceiptPercent = txIn / receiptTokenTotalSupply;
-      reservesAdded = reserves.map(reserve => reserve * additionalUserReceiptPercent);
+      seedTokensInvested = seedTokensInvested.map((prevInvestment, i) => prevInvestment + reserves[i] * additionalUserReceiptPercent);
       userReceiptBalanceAfterLastTx += txIn;
   
     } else if (txOut) {
-  
       // NOTE: case 1: the user is withdrawing liquidity - assumes that any receipt token transfer to the "withdraw" address is a withdrawal
       // NOTE: this obviously imperfect and needs to be improved
       const fieldWithdrawContract = field.contractAddresses.find(contractAddress => contractAddress.addressTypes.includes('withdraw'));
@@ -52,14 +55,14 @@ function calcEarningROI (currInvestmentValue, txHistory, field, tokenPrices) {
         const userPercentageofTotalSupplyB4Withdraw = userReceiptBalanceAfterLastTx / totalReceiptTokenSupplyB4Withdraw;
         const poolPercentageWithdrawn = txOut / totalReceiptTokenSupplyB4Withdraw;
         const percentOfUserBalanceWithdrawn = txOut / userReceiptBalanceAfterLastTx;
-        
+
         const totalReservesBeforeWithdraw = reserves.map(reserve => reserve / (1 - poolPercentageWithdrawn));
         const userReserveBalancesB4Withdraw = totalReservesBeforeWithdraw.map(totalReserve => totalReserve * userPercentageofTotalSupplyB4Withdraw);
-        const netReserveProfits = userReserveBalancesB4Withdraw.map((userReserveBalance, i) => (userReserveBalance - reservesAdded[i]) * percentOfUserBalanceWithdrawn);
-        reservesAdded = reservesAdded.map(reserve => reserve *= 1 - percentOfUserBalanceWithdrawn);
+        const netReserveProfits = userReserveBalancesB4Withdraw.map((userReserveBalance, i) => (userReserveBalance - seedTokensInvested[i]) * percentOfUserBalanceWithdrawn);
+        seedTokensInvested = seedTokensInvested.map(reserve => reserve *= 1 - percentOfUserBalanceWithdrawn);
         userReceiptBalanceAfterLastTx -= txOut;
         const pricesPerReserveToken = reserves.map(reserve => ((receiptTokenTotalSupply * pricePerToken) / 2) / reserve);
-        feeValueRealised += netReserveProfits[0] * pricesPerReserveToken[0] + netReserveProfits[1] * pricesPerReserveToken[1];
+        realisedRelativeProfitValue += netReserveProfits[0] * pricesPerReserveToken[0] + netReserveProfits[1] * pricesPerReserveToken[1];
         
         //NOTE: case 2: the user is simply transferring the token to another address
         //NOTE: fees are still considered realised, but no impact on field supplies/reserves after the tx
@@ -68,15 +71,26 @@ function calcEarningROI (currInvestmentValue, txHistory, field, tokenPrices) {
         const percentOfUserBalanceExited = txOut / userReceiptBalanceAfterLastTx;
         
         const userReserveBalancesB4Tx = reserves.map(reserve => reserve * userPercentageofTotalSupplyB4Tx);
-        const netReserveProfits = userReserveBalancesB4Tx.map((userReserveBalance, i) => (userReserveBalance - reservesAdded[i]) * percentOfUserBalanceExited);
-        reservesAdded = reservesAdded.map(reserve => reserve *= 1 - percentOfUserBalanceExited);
-        
+        const netReserveProfits = userReserveBalancesB4Tx.map((userReserveBalance, i) => (userReserveBalance - seedTokensInvested[i]) * percentOfUserBalanceExited);
+        seedTokensInvested = seedTokensInvested.map(reserve => reserve *= 1 - percentOfUserBalanceExited);
+
         userReceiptBalanceAfterLastTx -= txOut;
         const pricesPerReserveToken = reserves.map(reserve => ((receiptTokenTotalSupply * pricePerToken) / 2) / reserve);
-        feeValueRealised += netReserveProfits.reduce((acc, curr, i) => acc + (curr * pricesPerReserveToken[i]), 0);
+        realisedRelativeProfitValue += netReserveProfits.reduce((acc, curr, i) => acc + (curr * pricesPerReserveToken[i]), 0);
       }
     }
   }
-}
+
+  function calcUnrealisedRelativeProfit() {
+    const totalCurrentUserBalance = field.userBalance + field.stakedBalance.reduce((acc, curr) => acc + curr.balance, 0);
+    const userPoolPercentage = totalCurrentUserBalance / field.totalSupply;
+    const userFieldSeedHoldings = field.seedTokens.map(seedToken => seedToken.fieldReserve * userPoolPercentage);
+    const unrealisedProfitAmounts = userFieldSeedHoldings.map((userHolding, i) => userHolding - seedTokensInvested[i]);
+    unrealisedRelativeProfitValue = unrealisedProfitAmounts.reduce((acc, curr, i) => {
+      const tokenPrice = tokenPrices[field.seedTokens[i].name].usd;
+      return acc + curr * tokenPrice;
+    }, 0);
+  }
+ }
 
 export default calcEarningROI;
