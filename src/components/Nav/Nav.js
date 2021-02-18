587ed977c054e081d19cbecf10d016b65238397b
Raphael <raphael.mazet@gmail.com>
2021-02-18 13:48:42 +0000

    InfoModal: update information

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,17 +6,33 @@ import simpleFiLogo from '../../assets/logos/simplefi-logotype.svg';
 
 export default function Nav ({splash, userAccount, history}) {
   const [infoModalContent, setInfoModalContent] = useState('');
+  const [activeModal, setActiveModal] = useState(<></>);
   const aboutRef = useRef(null);
+  const smileyRef = useRef(null);
   const infoModalRef = useRef(null);
   const infoModalContentRef = useRef(null);
   const logoRef = useRef(null);
 
   function handleClick(e) {
     if (!infoModalContentRef.current.contains(e.target)) {
-      if (aboutRef.current.contains(e.target) && infoModalRef.current.style.visibility !== 'visible') {
-        setInfoModalContent('about');
-        infoModalRef.current.style.visibility = 'visible';
-        infoModalRef.current.style.opacity = 1;
+      
+      if (aboutRef.current.contains(e.target) || smileyRef.current.contains(e.target)) {
+        setActiveModal(e.target);
+      
+        if (infoModalRef.current.style.visibility !== 'visible' || e.target !== activeModal) {
+          infoModalRef.current.style.visibility = 'visible';
+          infoModalRef.current.style.opacity = 1;
+        } else {
+          infoModalRef.current.style.visibility = 'collapse';
+          infoModalRef.current.style.opacity = 0;
+        }
+
+        if (aboutRef.current.contains(e.target)) {
+          setInfoModalContent('about');
+        } else if (smileyRef.current.contains(e.target)) {
+          setInfoModalContent('😊');
+        }
+
       } else {
         infoModalRef.current.style.visibility = 'collapse';
         infoModalRef.current.style.opacity = 0;
@@ -40,7 +56,7 @@ export default function Nav ({splash, userAccount, history}) {
       <div className="nav-items">
         <div className="nav-links">
           <p ref={aboutRef} >About</p>
-          <p>😊</p>
+          <p ref={smileyRef} >😊</p>
           <a href="https://github.com/raphael-mazet/simplefi-client" target="_blank" rel="noreferrer">Github</a>
         </div>
           <div className="nav-address-button" style={!splash ? {visibility:'hidden'} : {}}>
